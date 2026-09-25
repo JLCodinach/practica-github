f8e85299b3d9438bdd74f8655ef3e7bde70f2385
JLCodinach <joan.codinach@campus.xaloc.org>
2026-09-25 09:42:08 +0200

    Add files via upload

---
+++ Ejercicio 2.py
@@ -0,0 +1,7 @@
+#Programa que introduzca por teclado tres tipos de variables y se muestren por pantalla en el siguiente orden: número entero, texto y número decimal.
+variable1=int(input("introduce un número:"))
+variable2=input("introduce una letra:")
+variable3=float(input("introduce un número decimal:"))
+print("El valor introducido es un",variable1)  
+print("El valor introducido es la letra:",variable2)
+print("El valor introducido es el número decimal:",variable3)
---
+++ Ejercicio 3.py
@@ -0,0 +1,5 @@
+#Programa que pida dos números enteros y realice la suma correspondiente 
+variable1=float(input("introduce el primer número:"))
+variable2=float(input("introduce el segundo número:"))
+
+print("La suma es:",variable1 + variable2)
